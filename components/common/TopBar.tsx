4ac534c384c72ef19ecb99c1b77893d0248e0b89
digitALL <ben@savassahin.com.tr>
2023-11-29 17:39:04 +0300

    Update TopBar.tsx

--- components/common/TopBar.tsx
+++ components/common/TopBar.tsx
@@ -34,7 +34,7 @@ const TopBar = ({ showSettings, showAddModal }:TopbarProps) => {
        ${isDomainsPage ? 'max-w-5xl lg:justify-between' : 'max-w-7xl lg:justify-end'}  bg-white lg:bg-transparent`}>
 
          <h3 className={`p-4 text-base font-bold text-blue-700 ${isDomainsPage ? 'lg:pl-0' : 'lg:hidden'}`}>
-            <span className=' relative top-[3px] mr-1'><Icon type="logo" size={24} color="#364AFF" /></span> SerpBear
+            <span className=' relative top-[3px] mr-1'><Icon type="logo" size={24} color="#364AFF" /></span> digiSERP
             <button className='px-3 py-1 font-bold text-blue-700  lg:hidden ml-3 text-lg' onClick={() => showAddModal()}>+</button>
          </h3>
          {!isDomainsPage && (
@@ -53,7 +53,7 @@ const TopBar = ({ showSettings, showAddModal }:TopbarProps) => {
             className={`text-sm font-semibold text-gray-500 absolute mt-[-10px] right-3 bg-white 
             border border-gray-200 lg:mt-2 lg:relative lg:block lg:border-0 lg:bg-transparent ${showMobileMenu ? 'block' : 'hidden'}`}>
                <li className='block lg:inline-block lg:ml-5'>
-                  <a className='block px-3 py-2 cursor-pointer' href='https://docs.serpbear.com/' target="_blank" rel='noreferrer'>
+                  <a className='block px-3 py-2 cursor-pointer' href='https://www.digitall.com.tr/' target="_blank" rel='noreferrer'>
                      <Icon type="question" color={'#888'} size={14} /> Help
                   </a>
                </li>
